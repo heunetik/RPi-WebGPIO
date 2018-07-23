ac14ba13dd53fca29608006a94f4a80a72585dfd
heunetik <heunetik@gmail.com>
2018-07-23 22:53:41 +0300

    Added project files

---
+++ routes/ajax.js
@@ -0,0 +1,15 @@
+const rpio = require('rpio');
+
+module.exports = function(req, res) {
+    if (req.body.hasOwnProperty('action')) {
+        switch( req.body.action) {
+            case 'write':
+                rpio.init({mapping: 'gpio'});
+                rpio.open(req.body.gpio, rpio.OUTPUT, + req.body.status);
+                rpio.write(req.body.gpio, + req.body.status);
+                res.contentType('json');
+                res.send({ gpio: req.body.gpio, status: req.body.status });
+            break;
+        }
+    }
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = 3000;
+
+const ajax = require('./routes/ajax');
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
+
+app.set('view engine', 'pug');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index', { title : 'Raspberry Web Gpio' });
+});
+
+app.post('/ajax', ajax);
